0d116af30fd3776996e0382159b4c59a15949c4e
Diego <20098757@mail.wit.ie>
2025-12-23 00:17:17 +0000

    Tasks related to User - lab completed!

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -1,34 +1,31 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import tasksRouter from './api/tasks';
-import './db/index.js';
 import cors from 'cors';
+
+import './db/index.js';
 import authenticate from './authenticate';
+
+import tasksRouter from './api/tasks';
 import usersRouter from './api/users';
+
 dotenv.config();
 
-
 const errHandler = (err, req, res, next) => {
-  /* if the error in development then send stack trace to display whole error,
-  if it's in production then just send error message  */
-  if(process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === 'production') {
     return res.status(500).send(`Something went wrong!`);
   }
   res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
 };
 
 const app = express();
-// Enable CORS for all requests
 app.use(cors());
-
-const port = process.env.PORT;
-
 app.use(express.json());
 
-app.use('/api/tasks', tasksRouter);
 app.use('/api/users', usersRouter);
-app.use(errHandler);  // - error handler must come AFTER r o u t e s
-app.use('/api/tasks', authenticate, tasksRouter);
-app.listen(port, () => {
-  console.info(`Server running at ${port}`);
+app.use('/api/tasks', authenticate, tasksRouter);  
+
+app.use(errHandler);  
+
+app.listen(process.env.PORT, () => {
+  console.info(`Server running at ${process.env.PORT}`);
 });
